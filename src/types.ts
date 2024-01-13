4503d718bf548f497238264e5a4128d3116bba48
na2na-p <na2na@na2na.dev>
2024-01-13 11:00:52 +0900

    Fix typo and create version 0.0.3

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,7 @@ export type SpotifyPlayerError = 'initialization_error' | 'authentication_error'
 export type SpotifyPlayerTrack = {
   uri: string, // Spotify URI
   id?: string, // Spotify ID from URI (can be null)
-  type: 'track' | 'episode' | 'add', // Content type: can be "track", "episode" or "ad"
+  type: 'track' | 'episode' | 'ad', // Content type: can be "track", "episode" or "ad"
   media_type: 'audio' | 'video', // Type of file: can be "audio" or "video"
   name: string, // Name of content
   is_playable: boolean, // Flag indicating whether it can be played
